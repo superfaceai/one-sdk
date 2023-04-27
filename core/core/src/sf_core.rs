7f380cbbadd5c992c01c198b8386c3ae2ddb2bdd
Ondřej Musil <o@superface.ai>
2023-04-27 10:20:12 +0200

    refactor(core): move prepare_security_map to security.rs

--- core/core/src/sf_core.rs
+++ core/core/src/sf_core.rs
@@ -10,16 +10,13 @@ use sf_std::unstable::{
     fs::{self, OpenOptions},
     http::HttpRequest,
     perform::{perform_input, perform_output, PerformOutput},
-    provider::{HttpSecurity, ProviderJson, SecurityScheme},
-    HostValue, SecurityValue, SecurityValuesMap,
+    provider::ProviderJson,
+    HostValue,
 };
 
 use interpreter_js::JsInterpreter;
 use map_std::{
-    unstable::{
-        security::{Security, SecurityMap},
-        MapValue,
-    },
+    unstable::{security::prepare_security_map, MapValue},
     MapInterpreter,
 };
 use tracing::instrument;
@@ -175,78 +172,6 @@ impl SuperfaceCore {
         }
     }
 
-    fn prepare_security_map(
-        provider_json: ProviderJson,
-        security_values: SecurityValuesMap,
-    ) -> SecurityMap {
-        let security_schemes = match provider_json.security_schemes {
-            Some(security_schemes) => security_schemes,
-            None => return SecurityMap::new(),
-        };
-
-        let mut security_map = SecurityMap::new();
-
-        for security_scheme in security_schemes {
-            match security_scheme {
-                SecurityScheme::ApiKey {
-                    id,
-                    r#in,
-                    name,
-                    body_type,
-                } => {
-                    let apikey = match security_values.get(&id) {
-                        Some(SecurityValue::ApiKey { apikey }) => apikey,
-                        Some(_) => continue, // TODO Error wrong value type
-                        None => continue, // TODO sentinel type to return error later in resolve_security
-                    };
-
-                    security_map.insert(
-                        id,
-                        Security::ApiKey {
-                            name,
-                            apikey: apikey.to_string(),
-                            r#in: map_std::unstable::security::ApiKeyPlacement::from(r#in),
-                            body_type: body_type
-                                .map(|bt| map_std::unstable::security::ApiKeyBodyType::from(bt)),
-                        },
-                    );
-                }
-                SecurityScheme::Http(HttpSecurity::Basic { id }) => {
-                    let (user, password) = match security_values.get(&id) {
-                        Some(SecurityValue::Basic { user, password }) => (user, password),
-                        Some(_) => continue, // TODO
-                        None => continue,    // TODO
-                    };
-
-                    security_map.insert(
-                        id,
-                        Security::Http(map_std::unstable::security::HttpSecurity::Basic {
-                            user: user.to_string(),
-                            password: password.to_string(),
-                        }),
-                    );
-                }
-                SecurityScheme::Http(HttpSecurity::Bearer { id, bearer_format }) => {
-                    let token = match security_values.get(&id) {
-                        Some(SecurityValue::Bearer { token }) => token,
-                        Some(_) => continue, // TODO
-                        None => continue,    // TODO
-                    };
-
-                    security_map.insert(
-                        id,
-                        Security::Http(map_std::unstable::security::HttpSecurity::Bearer {
-                            token: token.to_string(),
-                            bearer_format: bearer_format,
-                        }),
-                    );
-                }
-            }
-        }
-
-        security_map
-    }
-
     // TODO: use thiserror
     #[instrument(level = "Trace")]
     pub fn send_metrics(&mut self) -> anyhow::Result<()> {
@@ -278,7 +203,7 @@ impl SuperfaceCore {
         let provider_json = serde_json::from_slice::<ProviderJson>(provider_json)
             .context("Failed to deserialize provider JSON")?;
 
-        let map_security = Self::prepare_security_map(provider_json, perform_input.map_security); // TODO SecurityMap
+        let map_security = prepare_security_map(provider_json, perform_input.map_security); // TODO SecurityMap
 
         // let mut profile_validator = ProfileValidator::new(
         //     std::str::from_utf8(
--- core/core_to_map_std/src/unstable/security.rs
+++ core/core_to_map_std/src/unstable/security.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+
+use sf_std::unstable::{provider::ProviderJson, SecurityValue, SecurityValuesMap};
 pub enum ApiKeyPlacement {
     Header,
     Body,
@@ -64,3 +66,78 @@ pub enum Security {
 }
 
 pub type SecurityMap = HashMap<String, Security>;
+
+pub fn prepare_security_map(
+    provider_json: ProviderJson,
+    security_values: SecurityValuesMap,
+) -> SecurityMap {
+    let security_schemes = match provider_json.security_schemes {
+        Some(security_schemes) => security_schemes,
+        None => return SecurityMap::new(),
+    };
+
+    let mut security_map = SecurityMap::new();
+
+    for security_scheme in security_schemes {
+        match security_scheme {
+            sf_std::unstable::provider::SecurityScheme::ApiKey {
+                id,
+                r#in,
+                name,
+                body_type,
+            } => {
+                let apikey = match security_values.get(&id) {
+                    Some(SecurityValue::ApiKey { apikey }) => apikey,
+                    Some(_) => continue, // TODO Error wrong value type
+                    None => continue, // TODO sentinel type to return error later in resolve_security
+                };
+
+                security_map.insert(
+                    id,
+                    Security::ApiKey {
+                        name,
+                        apikey: apikey.to_string(),
+                        r#in: ApiKeyPlacement::from(r#in),
+                        body_type: body_type.map(|bt| ApiKeyBodyType::from(bt)),
+                    },
+                );
+            }
+            sf_std::unstable::provider::SecurityScheme::Http(
+                sf_std::unstable::provider::HttpSecurity::Basic { id },
+            ) => {
+                let (user, password) = match security_values.get(&id) {
+                    Some(SecurityValue::Basic { user, password }) => (user, password),
+                    Some(_) => continue, // TODO
+                    None => continue,    // TODO
+                };
+
+                security_map.insert(
+                    id,
+                    Security::Http(HttpSecurity::Basic {
+                        user: user.to_string(),
+                        password: password.to_string(),
+                    }),
+                );
+            }
+            sf_std::unstable::provider::SecurityScheme::Http(
+                sf_std::unstable::provider::HttpSecurity::Bearer { id, bearer_format },
+            ) => {
+                let token = match security_values.get(&id) {
+                    Some(SecurityValue::Bearer { token }) => token,
+                    Some(_) => continue, // TODO
+                    None => continue,    // TODO
+                };
+
+                security_map.insert(
+                    id,
+                    Security::Http(HttpSecurity::Bearer {
+                        token: token.to_string(),
+                        bearer_format: bearer_format,
+                    }),
+                );
+            }
+        }
+    }
+
+    security_map
+}
