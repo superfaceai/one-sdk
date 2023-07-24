041f7e89d582e9ac59f23fd639f2b34359acbbb9
Ondřej Musil <o@superface.ai>
2023-07-24 11:38:03 +0200

    --wip-- [skip ci]

--- core/schemas/src/security_values.rs
+++ core/schemas/src/security_values.rs
@@ -1,4 +1,4 @@
-// Failed pretty fast on HashMap support: https://github.com/GREsau/schemars/issues/124
+// https://github.com/GREsau/schemars/issues/124
 
 /*
 {
@@ -43,15 +43,62 @@
 }
 */
 
-//use std::collections::HashMap;
+use core::fmt;
+use std::collections::BTreeMap;
 
-//pub type SecurityValues = HashMap<String, SecurityValue>;
+use schemars::{
+    schema::{InstanceType, ObjectValidation, Schema, SchemaObject, StringValidation},
+    schema_for, JsonSchema,
+};
 
-use schemars::JsonSchema;
+#[derive(Debug, Eq, Ord)]
+pub struct Id {
+    value: String,
+}
+impl From<&str> for Id {
+    fn from(s: &str) -> Id {
+        Id {
+            value: s.to_owned(),
+        }
+    }
+}
+impl Into<Id> for String {
+    fn into(self) -> Id {
+        Id { value: self }
+    }
+}
+impl fmt::Display for Id {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.value.fmt(f)
+    }
+}
+impl PartialEq for Id {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
+}
+impl PartialOrd for Id {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.value.partial_cmp(&other.value)
+    }
+}
 
-#[derive(JsonSchema)]
-pub struct SecurityValues {
-    pub name: SecurityValue,
+impl JsonSchema for Id {
+    fn schema_name() -> String {
+        "Id".to_owned()
+    }
+
+    fn json_schema(_gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+        SchemaObject {
+            instance_type: Some(InstanceType::String.into()),
+            string: Some(Box::new(StringValidation {
+                pattern: Some(r"^[a-z][_\-a-z]*$".to_owned()),
+                ..Default::default()
+            })),
+            ..Default::default()
+        }
+        .into()
+    }
 }
 
 #[derive(JsonSchema)]
@@ -59,4 +106,63 @@ pub enum SecurityValue {
     Apikey { apikey: String },
     Token { token: String },
     Basic { username: String, password: String },
+}
+
+pub struct SecurityValues {
+    map: BTreeMap<Id, SecurityValue>,
+}
+impl SecurityValues {
+    pub fn new() -> Self {
+        Self {
+            map: BTreeMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, key: Id, value: SecurityValue) {
+        self.map.insert(key, value);
+    }
+}
+impl JsonSchema for SecurityValues {
+    fn schema_name() -> String {
+        "SecurityValues".to_owned()
+    }
+
+    fn json_schema(_gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+        let mut pattern_properties = BTreeMap::new();
+        pattern_properties.insert(
+            schema_for!(Id).schema.string.unwrap().pattern.unwrap(),
+            Schema::Object(schema_for!(SecurityValue).schema),
+        );
+
+        SchemaObject {
+            instance_type: Some(InstanceType::Object.into()),
+            object: Some(Box::new(ObjectValidation {
+                pattern_properties,
+                ..Default::default()
+            })),
+            ..Default::default()
+        }
+        .into()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use schemars::schema_for;
+
+    use super::{Id, SecurityValue, SecurityValues};
+
+    #[test]
+    fn basics() {
+        let mut sec_vals = SecurityValues::new();
+        sec_vals.insert(
+            "example_api_key".into(),
+            SecurityValue::Apikey {
+                apikey: "my api key".to_owned(),
+            },
+        );
+
+        let schema = schema_for!(SecurityValues);
+        println!("{}", serde_json::to_string_pretty(&schema).unwrap());
+    }
 }
