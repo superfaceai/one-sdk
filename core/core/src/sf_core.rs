21b88c9e4d1a67752737f81800f69a2dea7c4225
Ondřej Musil <o@superface.ai>
2023-07-25 12:18:13 +0200

    refactor: metrics struct to own module

--- core/core/src/sf_core.rs
+++ core/core/src/sf_core.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Cow, collections::BTreeMap, str::FromStr};
+use std::{collections::BTreeMap, str::FromStr};
 
 use sf_std::unstable::{
     exception::{PerformException, PerformExceptionErrorCode},
@@ -15,7 +15,7 @@ use map_std::unstable::{
 
 use crate::{
     bindings::{MessageExchangeFfi, StreamExchangeFfi},
-    sf_core::json_schema_validator::JsonSchemaValidator,
+    sf_core::{json_schema_validator::JsonSchemaValidator, metrics::PerformMetricsData},
 };
 
 mod cache;
@@ -24,6 +24,7 @@ mod digest;
 mod exception;
 mod json_schema_validator;
 mod map_std_impl;
+mod metrics;
 mod profile_validator;
 
 // use crate::profile_validator::ProfileValidator;
@@ -36,58 +37,6 @@ use self::cache::{MapCacheEntry, ProfileCacheEntry, ProviderJsonCacheEntry};
 type Fs = sf_std::unstable::fs::FsConvenience<MessageExchangeFfi, StreamExchangeFfi>;
 type HttpRequest = sf_std::unstable::http::HttpRequest<MessageExchangeFfi, StreamExchangeFfi>;
 type IoStream = sf_std::unstable::IoStream<StreamExchangeFfi>;
-
-/// For the purposes of metrics we are interested in some data which may or may not be parsed out of the profile, provider and map.
-///
-/// If the perform ends successfully or with a maped error these fields will be available. If it ends with an exception some of these fields might not be available.
-#[derive(Debug, Default)]
-#[allow(dead_code)] // TODO: until we use these fields
-struct PerformMetricsData<'a> {
-    /// Profile id in format `<scope>/<name>`
-    pub profile: Option<&'a str>,
-    /// Profile url as passed into perform
-    pub profile_url: &'a str,
-    /// Profile version as parsed from the profile header
-    pub profile_version: Option<String>,
-    pub profile_content_hash: Option<&'a str>,
-    /// Provider name
-    pub provider: Option<&'a str>,
-    /// Provider url as passed into perform
-    pub provider_url: &'a str,
-    pub provider_content_hash: Option<&'a str>,
-    /// Map url as passed into perform
-    pub map_url: &'a str,
-    /// Map version as parsed from map metadata
-    pub map_version: Option<String>,
-    pub map_content_hash: Option<&'a str>,
-}
-impl<'a> PerformMetricsData<'a> {
-    pub fn get_profile(&self) -> Cow<'_, str> {
-        match self.profile {
-            Some(profile) => Cow::Borrowed(profile),
-            None => Cow::Owned(
-                self.profile_url
-                    .split('/')
-                    .last()
-                    .and_then(|b| b.strip_suffix(".profile"))
-                    .unwrap()
-                    .replace('.', "/"),
-            ),
-        }
-    }
-
-    pub fn get_provider(&self) -> &str {
-        match self.provider {
-            Some(provider) => provider,
-            None => self
-                .provider_url
-                .split('/')
-                .last()
-                .and_then(|b| b.strip_suffix(".provider.json"))
-                .unwrap(),
-        }
-    }
-}
 
 #[derive(Debug)]
 pub struct OneClientCore {
