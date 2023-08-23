51f05bd1dfa7fcb4976fd4db688e3d6856acc7bf
Jan Halama <mail@janhalama.cz>
2023-08-23 14:00:43 +0200

    fix: remove explicit javascript exports

--- host/javascript/src/common/index.ts
+++ host/javascript/src/common/index.ts
@@ -1,4 +1,4 @@
-export { App } from './app.js';
+export { App, AsyncMutex } from './app.js';
 export type { TextCoder, FileSystem, Timers, Network, WasiContext, Persistence } from './interfaces.js';
 export { HandleMap } from './handle_map.js';
 export { SecurityValuesMap } from './security.js';
