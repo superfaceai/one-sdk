445e0734f42e8a02bf324a3ad91ea71a78b55920
TheEdward162 <theedward162@gmail.com>
2024-03-12 17:42:56 +0100

    chore: update Javy dependency
    * also correct typos in examples

--- examples/comlinks/src/wasm-sdk.example.profile.ts
+++ examples/comlinks/src/wasm-sdk.example.profile.ts
@@ -2,11 +2,11 @@
 type Example = UseCase<{
   safety: 'safe'
   input: { id: AnyValue }
-  result: { url: AnyValue, method: AnyValue, query: AnyValue, headers: AnyValue, x?: 1, y: true, z: "seven" }
+  result: { url: AnyValue, method: AnyValue, query: AnyValue, headers: AnyValue }
   error: { title: AnyValue, detail?: AnyValue, r: boolean }
 }>;
 
 const exampleExamples: Example['examples'] = [
-  { input: { id: "a" }, result: { url: 1, method: 2, query: 3, headers: 4, x: 1, y: true, z: 'seven' } },
+  { input: { id: "a" }, result: { url: 1, method: 2, query: 3, headers: 4 } },
   { input: { id: 1 }, error: { title: "hi", r: true } }
 ]
